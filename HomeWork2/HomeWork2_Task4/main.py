6b8892d778d5c07fa547b151d2a33aebfacb3c8f
Artyom <artyom_kudryavcev_91@mail.ru>
2023-02-19 12:24:29 +0700

    Добавил решение задачи №4 домашней работы №2

--- HomeWork2/HomeWork2_Task4/main.py
+++ HomeWork2/HomeWork2_Task4/main.py
@@ -2,4 +2,19 @@
 #(т.е. числа вида 2k), не превосходящие числа N.
 #Пример 10: 0, 1, 2, 3
 
+print('Введите целое натуральное число: ')
+inputNumber = int(input())
+
+list1 = []
+i = 0
+while True:
+    two = 2
+    two **= i
+    if two > inputNumber:
+        break
+    list1.append(i)
+    i+=1
+print(list1)
+
+
 
