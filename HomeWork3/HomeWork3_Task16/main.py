0d37e62bbc0fcb1474bd20de90d19f3b45e2e97c
Artyom <artyom_kudryavcev_91@mail.ru>
2023-02-19 13:06:24 +0700

    Добавил код решения задачи №16 домашней работы №3

--- HomeWork3/HomeWork3_Task16/main.py
+++ HomeWork3/HomeWork3_Task16/main.py
@@ -8,3 +8,24 @@
 3
 -> 1"""
 
+print('Введите количество чисел в массиве: ')
+numberOfNumberArray = int(input())
+list1 = []
+for i in range(numberOfNumberArray):
+    print(f'Введите число натуральное число № {i+1}')
+    list1.append(int(input()))
+print('Введите проверяемое число:')
+numberToCheck = int(input())
+count = 0
+for i in range(len(list1)):
+    if int(list1[i]) == numberToCheck:
+        count+=1
+        break
+print(list1)
+if count == 0:
+    print('Число не найдено!')
+else:
+    print(count)
+
+
+
